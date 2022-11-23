de1d6bd56577f96a006c7ed8554c39ebe0f5ce1a
Alejandro Domin <alejandrodirm52@gmail.com>
2022-11-23 19:54:11 +0100

    los dos en el mismo codigo

--- 01ModulosCore/02-http-server.js
+++ 01ModulosCore/02-http-server.js
@@ -1,3 +1,19 @@
+/*
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+*/
 var http = require('http') // Con require vamos a llamar al módulo interno de Node en cuestión. 
 
 function webServer(req, res)
