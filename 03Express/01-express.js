a80221279e316a2ee63c2dd8cacae469dbdc61ec
Alejandro Domin <alejandrodirm52@gmail.com>
2023-01-11 19:18:24 +0100

    ok

--- 03Express/01-express.js
+++ 03Express/01-express.js
@@ -1,26 +1,27 @@
 const express = require('express')
 const app = express()
 const port = 3005
-//motor de plantilla
-app.set('view engine','ejs');
-app.set('views', __dirname+'/views');
-//middleware
-app.use(express.static(__dirname + '/public'));
-app.get('/', (req, res) => {
-  res.render("index",{titulo:"mi titulo dinamico"})
-})
-//peticiones basicas
-app.get('/contacto', (req, res) => {
-    res.render('contacto',{tituloContacto:"es mi contacto dinamico"});
-})
-app.use((req,res) => {
-    res.status(404).render("404",{
-        titulo:"error 404",
-        descripcion:"Page Not Found"
 
+// Motor de plantillas
+app.set("views", __dirname + "/views") // Añadir la constante __dirname es una buena práctica para que funcione en todos los entornos globales
+app.set("view engine", "ejs")
+
+// Middleware
+app.use(express.static(__dirname + "/public")) // Importante
+
+// Llamadas a las rutas
+app.use("/", require("./routes/rutas"));
+app.use("/pokemon", require("./routes/pokemon"));
+
+// Si no se encuentra el recurso (Error 404) con página personalizada
+app.use((req, res) => {
+    //res.status(404).sendFile(__dirname + "/public/404.html")
+    res.status(404).render("404", {
+        titulo: "Error 404",
+        descripcion: "Page Not Found"
     })
- })
-app.use(express.static(__dirname + '/public'));
+})
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
